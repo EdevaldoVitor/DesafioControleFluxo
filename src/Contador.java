bfda5982af000e342fdadb98947f6ef5580d9460
EdevaldoVitor <vitor.cohene@hotmail.com>
2025-06-19 16:01:18 -0300

    feat: Implementa solução para o desafio proposto.

--- src/Contador.java
+++ src/Contador.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Contador {
 
     public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
 
-
-
-
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (Exception e) {
+            throw new ParametrosInvalidosException();
+        }
 
     }
-
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.printf("Imprimindo o número %s \n", i);
+        }
+    }
 }
+
